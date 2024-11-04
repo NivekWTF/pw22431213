a2e6b98b789a4d1df3b0f07059eb06283b867176
NivekWTF <nivekgamer2015@gmail.com>
2024-11-03 23:01:58 -0700

    Creacion de tarjetas de personas usando API de randomusers

--- API - Fetch/js/personas.js
+++ API - Fetch/js/personas.js
@@ -1,22 +1,47 @@
-const url = 'https://randomuser.me/api/';
+const url = 'https://randomuser.me/api/?results=12';
 let btn = document.querySelector('button');
-let foto = document.querySelector('#foto');
-let nombre = document.querySelector('#nombre');
-let direccion = document.querySelector('#direccion');
-let telefono = document.querySelector('#telefono');
+let lista = document.querySelector('#lista-personas');
+// let foto = document.querySelector('#foto');
+// let nombre = document.querySelector('#nombre');
+// let direccion = document.querySelector('#direccion');
+// let telefono = document.querySelector('#telefono');
+
+const agregarPersonasLista = (persona) => {
+    lista.classList.add('lista-personas'); 
+    const tarjeta = document.createElement('article');
+    tarjeta.classList.add('tarjeta');
+
+    const foto = document.createElement('img');
+    foto.src = persona.picture.large;
+
+    const nombre = document.createElement('article');
+    nombre.textContent = persona.name.first + ' ' + persona.name.last;
+
+    const direccion = document.createElement('article');
+    direccion.textContent = persona.location.street.name + ', ' 
+    + persona.location.street.number;
+
+    const telefono = document.createElement('article');
+    telefono.textContent = persona.phone;
+
+    tarjeta.appendChild(foto);
+    tarjeta.appendChild(nombre);
+    tarjeta.appendChild(direccion);
+    tarjeta.appendChild(telefono);
+    lista.appendChild(tarjeta);
+    
+}
+
 
 const obtenerPersona = () => {
+    //Limpio la lista de personas antes de agregar nuevas
+    lista.innerHTML = '';
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(respuesta => {
             // console.log(respuesta);
             respuesta.results.forEach(persona => {
-                nombre.textContent = persona.name.first + ' ' + persona.name.last;
-                foto.src = persona.picture.large;
-                direccion.textContent = persona.location.street.name + ', ' + 
-                persona.location.street.number;
-                telefono.textContent = persona.phone;
-                
+                agregarPersonasLista(persona);
             })
     })
 }
